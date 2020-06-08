dd3622860fe540ff34c188a605c49cffa07eeb73
Do Merchán <34308597+domerchan@users.noreply.github.com>
2020-06-08 13:44:51 -0500

    Update CorreccionMultiprocessing.py

--- CorreccionMultiprocessing.py
+++ CorreccionMultiprocessing.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     
     
     inicioPar = time.time()
-    pool = multiprocessing.Pool(processes=20)
+    pool = multiprocessing.Pool(processes=25)
     resultsPar = pool.map(how_many_within_range_parallel, ar)
     pool.close() 
     pool.join()  
